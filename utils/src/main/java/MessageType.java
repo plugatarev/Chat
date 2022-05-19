344f9be922b4d8a0cd44cc4dc302c4017fae2a08
plugatarev <plugatarevk@gmail.com>
2022-05-19 15:04:17 +0700

    util package

---
+++ utils/src/main/java/MessageType.java
@@ -0,0 +1,8 @@
+public enum MessageType {
+    SHOW_USERS,
+    SEND_EVERYBODY,
+    SEND_USER,
+    EXIT,
+    REGISTRATION,
+    NOT_REGISTRATION
+}
---
+++ utils/src/main/java/User.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public final class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (User) obj;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
