74e5b1754814631de1f57e42ef4e8cdc7865bc07
plugatarev <plugatarevk@gmail.com>
2022-06-12 09:36:30 +0700

    added show of own messages

--- client/src/main/java/View.java
+++ client/src/main/java/View.java
@@ -1,10 +1,11 @@
 public record View() {
-    public static void update(Message m) {
+    public static void update(Message m, String userLogin) {
         switch (m.type()) {
             case REGISTRATION -> System.out.print("New user: ");
             case SHOW_USERS -> System.out.println("Users online:");
             case SEND_USER, SEND_EVERYBODY -> {
-                if (m.senderName() != null) System.out.print(m.senderName() + ": ");
+                if (userLogin.equals(m.senderName())) System.out.print("> ");
+                else System.out.print(m.senderName() + ": ");
             }
         }
         System.out.println(m.message());
