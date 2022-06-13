cfc96c0bf870e332646a36c2a5b35d4bf616b8c8
plugatarev <plugatarevk@gmail.com>
2022-06-13 16:53:41 +0700

    List<Client> -> HashMap<name, writer>

--- server/src/main/java/ClientService.java
+++ server/src/main/java/ClientService.java
@@ -2,71 +2,42 @@ import java.util.*;
 
 public class ClientService{
 
-    private final HashSet<Client> clients = new HashSet<>();
+    private final HashMap<String, Writer> clients = new HashMap<>();
 
     public synchronized void sendAll(BroadMessage message) {
-        for (Client c : clients){
-            message.setReceiverName(c.name());
+        for (Map.Entry<String, Writer> c : clients.entrySet()){
+            message.setReceiverName(c.getKey());
             if (message.type() == MessageType.SEND_EVERYBODY) message.setMessageType(MessageType.SEND_USER);
-            Writer writer = c.writer();
-            writer.write(new Message(message.message(), message.type(), message.senderName(), c.name()));
+            Writer writer = c.getValue();
+            writer.write(new Message(message.message(), message.type(), message.senderName(), c.getKey()));
         }
     }
 
     public synchronized void sendTo(UserMessage message) {
         String receiver = message.receiverName();
         Writer writer;
-        if (!clients.contains(new Client(receiver, null))){
-            writer = getWriter(message.senderName());
+        if (!clients.containsKey(receiver)){
+            writer = clients.get(message.senderName());
             message = new UserMessage("Such user doesn't exists", MessageType.SEND_USER, null, message.senderName());
         }
         else{
-            writer = getWriter(receiver);
+            writer = clients.get(receiver);
         }
         writer.write(message);
     }
 
     public synchronized boolean register(String clientName, Writer outputStream) {
-        if (clients.contains(new Client(clientName, null))) return false;
-        clients.add(new Client(clientName, outputStream));
+        if (clients.containsKey(clientName)) return false;
+        clients.put(clientName, outputStream);
         return true;
     }
 
     public synchronized List<String> getClientNames(){
-        return clients.stream().map(Client::name).toList();
+        return clients.keySet().stream().toList();
     }
 
     public synchronized void delete(String clientName) {
-        Client client = null;
-        for (Client c : clients){
-            if (c.name().equals(clientName)){
-                client = c;
-                break;
-            }
-        }
-        if (client != null) clients.remove(client);
-    }
-
-    private Writer getWriter(String client){
-        for (Client c : clients){
-            if (c.name().equals(client)) return c.writer();
-        }
-        return null;
-    }
-
-    // CR: override equals and hashcode
-    private record Client(String name, Writer writer) {
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == this) return true;
-            if (obj == null || obj.getClass() != this.getClass()) return false;
-            var that = (Client) obj;
-            return this.name.equals(that.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return name.hashCode();
-        }
+        //TODO: if doesn't exists
+        clients.remove(clientName);
     }
 }
